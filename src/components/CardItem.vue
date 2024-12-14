9a28f991c047ca742f78c59899bb312fe9a282f1
Laurelise Santos <laurelisesantos@MacBook-Air-de-Laurelise.local>
2024-12-14 09:54:05 -0300

    first commit

---
+++ src/components/CardItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const pokemon = defineProps(['info'])
+
+    const urlDetails = `http://localhost:8000/api/v1/pokemons/${pokemon.info.id}`
+    
+</script>
+
+<template>
+    <div>
+        <div class="card">
+            <p>{{ pokemon.info.name }}</p>
+            <a :href="urlDetails">
+                <button class="btn-details">Detalhes</button>    
+            </a>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { onMounted, reactive} from 'vue';
+    import CardItem from '@/components/CardItem.vue';
+    
+    let pokemons = reactive();
+
+    onMounted(() => {
+        fetch("http://localhost:8000/api/v1/pokemons?offset=2&limit=5")
+        .then(res => res.json())
+        .then(res => pokemons = res);
+    })    
+</script>
+
+<template>
+  <div class="section">
+    <CardItem v-for="pokemon in pokemons.data.items" :key="pokemon.name" :info="pokemon" />
+  </div>
+</template>
